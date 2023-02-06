e8ca6e8518e505cedb7e0b5e6186eb87c51b0d99
Snoopawoo <reki.csaba@gmail.com>
2023-02-06 21:42:54 +0000

    adds basic contact page

--- react_portfolio/src/pages/Contact.js
+++ react_portfolio/src/pages/Contact.js
@@ -1,10 +1,16 @@
 function Contact() {
     return (
-        <>
-            <h1>Contact</h1>
+        <div class = 'contactpage'>
+            <h1 class = 'ContactTitle'>Contact Information</h1>
 
-            <h1>dawdawdawdawdwadawWADAW</h1>
-        </>
+            <ul class="list-group groupb">
+                <li class="list-group-item"><a href = '#'>Github</a><p>https://github.com/Snoopawoo</p></li>
+                <li class="list-group-item"><a href = '#'>LinkedIn</a><p>https://www.linkedin.com/in/csaba-rektenwald-9301541b5/</p></li>
+                <li class="list-group-item"><a href = '#'>E-mail</a><p>reki.csaba@gmail.com</p></li>
+                <li class="list-group-item"><a href = '#'>Phone UK</a><p>+44 7546 616255</p></li>
+                <li class="list-group-item"><a href = '#'>Phone HU</a><p>+36 30 811 2690</p></li>
+            </ul>
+        </div>
     )
 }
 
